07ea8da78c90177831b6f607ffc5f2c406e91d64
0xRomRom <roman0811@live.nl>
2024-11-23 03:02:13 +0100

    save

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 
 const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route index path="/" element={<Home />} />
-      </Routes>
-    </>
-  );
+  return <Home />;
 };
 
 export default App;
